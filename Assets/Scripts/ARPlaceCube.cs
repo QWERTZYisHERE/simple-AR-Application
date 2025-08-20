31022eec7e759171f4d63962e9d7d72e36d4bc3b
QWERTZYisHERE <153390139+QWERTZYisHERE@users.noreply.github.com>
2025-08-20 10:55:52 +0200

    unity cubes und andere formen

--- Assets/Scripts/ARPlaceCube.cs
+++ Assets/Scripts/ARPlaceCube.cs
@@ -3,45 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UIElements;
 
-public class ARPlaceCube : MonoBehaviour
+public class ARPlaceObjectUI : MonoBehaviour
 {
     [SerializeField] private ARRaycastManager raycastManager;
-    bool isPlacing = false;
 
-    // Update is called once per frame
+    [SerializeField] private GameObject cubePrefab;
+    [SerializeField] private GameObject spherePrefab;
+    [SerializeField] private GameObject cylinderPrefab;
+
+    private GameObject selectedPrefab;
+    private bool isPlacing = false;
+
+    // Keep track of buttons to highlight active one
+    private Button cubeButton;
+    private Button sphereButton;
+    private Button cylinderButton;
+
+    void Start()
+    {
+
+
+
+        selectedPrefab = cubePrefab; // default
+
+        // Get UI
+        var uiDocument = FindObjectOfType<UIDocument>();
+        if (uiDocument != null)
+        {
+            var root = uiDocument.rootVisualElement;
+
+            cubeButton = root.Q<Button>("CubeButton");
+            sphereButton = root.Q<Button>("SphereButton");
+            cylinderButton = root.Q<Button>("CylinderButton");
+
+            if (cubeButton != null) cubeButton.clicked += () => SelectPrefab(cubePrefab, cubeButton);
+            if (sphereButton != null) sphereButton.clicked += () => SelectPrefab(spherePrefab, sphereButton);
+            if (cylinderButton != null) cylinderButton.clicked += () => SelectPrefab(cylinderPrefab, cylinderButton);
+
+            HighlightActive(cubeButton); // highlight default
+
+
+            Debug.Log($"CubeButton found? {cubeButton != null}");
+            Debug.Log($"SphereButton found? {sphereButton != null}");
+            Debug.Log($"CylinderButton found? {cylinderButton != null}");
+        }
+
+    }
+
     void Update()
     {
         if (!raycastManager) return;
 
         if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began ||
-            Input.GetMouseButtonDown(0)) && !isPlacing)
+             Input.GetMouseButtonDown(0)) && !isPlacing)
         {
             isPlacing = true;
 
-            if (Input.touchCount > 0)
-            {
-                PlaceObject(Input.GetTouch(0).position);
-            }
-            else
-            {
-                PlaceObject(Input.mousePosition);
-            }
+            Vector2 touchPosition = (Input.touchCount > 0) ?
+                Input.GetTouch(0).position :
+                (Vector2)Input.mousePosition;
+
+            PlaceObject(touchPosition);
         }
     }
 
     void PlaceObject(Vector2 touchPosition)
     {
         var rayHits = new List<ARRaycastHit>();
-        raycastManager.Raycast(touchPosition, rayHits, TrackableType.AllTypes);
+        raycastManager.Raycast(touchPosition, rayHits, TrackableType.PlaneWithinPolygon);
 
-        if (rayHits.Count > 0)
+        if (rayHits.Count > 0 && selectedPrefab != null)
         {
-            Vector3 hitPosePosition = rayHits[0].pose.position;
-            Quaternion hitPoseRotation = rayHits[0].pose.rotation;
-            Instantiate(raycastManager.raycastPrefab, hitPosePosition, hitPoseRotation);
+            Pose hitPose = rayHits[0].pose;
+            Instantiate(selectedPrefab, hitPose.position, hitPose.rotation);
         }
-        
+
         StartCoroutine(SetIsPlacingToFalseWithDelay());
     }
 
@@ -49,5 +87,23 @@ public class ARPlaceCube : MonoBehaviour
     {
         yield return new WaitForSeconds(0.25f);
         isPlacing = false;
+    }
+
+    void SelectPrefab(GameObject prefab, Button activeButton)
+    {
+        selectedPrefab = prefab;
+        Debug.Log($"Selected prefab: {prefab.name}");
+        HighlightActive(activeButton);
+    }
+
+    void HighlightActive(Button active)
+    {
+        // Reset all
+        if (cubeButton != null) cubeButton.style.backgroundColor = new StyleColor(Color.white);
+        if (sphereButton != null) sphereButton.style.backgroundColor = new StyleColor(Color.white);
+        if (cylinderButton != null) cylinderButton.style.backgroundColor = new StyleColor(Color.white);
+
+        // Highlight chosen one
+        if (active != null) active.style.backgroundColor = new StyleColor(Color.green);
     }
 }
